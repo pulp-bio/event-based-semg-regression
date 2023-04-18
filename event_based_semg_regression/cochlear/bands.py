b4189dced9ebb705daac01264525853cbfcd4930
MarcelloZanghieri2 <59026506+MarcelloZanghieri2@users.noreply.github.com>
2023-04-18 13:24:44 +0200

    Add files via upload

---
+++ event_based_semg_regression/cochlear/bands.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+
+import numpy as np
+import scipy.signal as ssg
+import matplotlib.pyplot as plt
+
+
+ORDER = 4
+BANDS_HZ_LIST = [
+    (0.0, 32.1),
+    (32.1, 119.2),
+    (119.2, 356.1),
+    (356.1, 1000.0),
+]
+NUM_BANDS = len(BANDS_HZ_LIST)
+
+
+def create_butterworth_bank(
+    f_hz: float,
+    bands_hz_list: list[float],
+    order: int,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+    num_bands = len(bands_hz_list)
+
+    z_bank = [None for idx_band in range(num_bands)]
+    p_bank = [None for idx_band in range(num_bands)]
+    k_bank = np.zeros((num_bands,), dtype=np.float64)
+    sos_bank = [None for idx_band in range(num_bands)]
+
+    for idx_band in range(num_bands):
+
+        fnyquist_hz = f_hz / 2.0
+        lowcut_hz, highcut_hz = bands_hz_list[idx_band]
+        lowcut_norm = lowcut_hz / fnyquist_hz
+        highcut_norm = highcut_hz / fnyquist_hz
+
+        if lowcut_norm == 0.0 and highcut_norm == 1.0:
+            raise NotImplementedError("Band %d is all-pass: no filtering!")
+        elif lowcut_norm == 0.0 and highcut_norm < 1.0:
+            Wn = highcut_norm
+            btype = 'lowpass'
+        elif lowcut_norm > 0.0 and highcut_norm < 1.0:
+            Wn = [lowcut_norm, highcut_norm]
+            btype = 'bandpass'
+        elif lowcut_norm > 0.0 and highcut_norm == 1.0:
+            Wn = lowcut_norm
+            btype = 'highpass'
+
+        z_bank[idx_band], p_bank[idx_band], k_bank[idx_band] = \
+            ssg.butter(N=order, Wn=Wn, btype=btype, output='zpk')
+
+        sos_bank[idx_band] = \
+            ssg.zpk2sos(z_bank[idx_band], p_bank[idx_band], k_bank[idx_band])
+
+    return sos_bank, z_bank, p_bank, k_bank
+
+
+def visualize_bandfilter_bank(
+    z_bank: np.ndarray,
+    p_bank: np.ndarray,
+    k_bank: np.ndarray,
+    norm_freq: bool = True,
+    fs_hz: float = None,
+    num_freqs: int | None = None,
+    xscale: str = 'linear',
+    colors: list[str | tuple[float, float, float]] | None = None,
+) -> None:
+
+    num_bands = len(k_bank)
+    w_bank = np.zeros((num_bands, num_freqs), dtype=np.float64)
+    h_bank = np.zeros((num_bands, num_freqs), dtype=np.complex128)
+
+    if norm_freq:
+        fs = 2.0
+        freq_axis_label = "Normalized frequency (dimensionless)"
+    else:
+        fs = fs_hz
+        freq_axis_label = "Frequency (Hz)"
+
+    xlim_lo = 0.0 if xscale == 'linear' else None
+    xlim_hi = fs / 2.0
+
+    for idx_band in range(num_bands):
+        w_bank[idx_band], h_bank[idx_band] = ssg.freqz_zpk(
+            z=z_bank[idx_band],
+            p=p_bank[idx_band],
+            k=k_bank[idx_band],
+            worN=num_freqs,
+            whole=False,
+            fs=fs,
+        )
+    abs_h_bank = np.abs(h_bank)
+    abs_db_h_bank = 20.0 * np.log10(abs_h_bank)
+    angle_h_bank = np.angle(h_bank)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(
+        nrows=3, ncols=1, sharex=True, figsize=(8.0, 8.0)
+    )
+    fig.suptitle("Frequency response of the band filters bank")
+    ax1.set_title("Amplitude (linear)")
+    ax2.set_title("Amplitude (logarithmic)")
+    ax3.set_title("Phase")
+
+    ax1.set_ylabel("Gain (dimensionless)")
+    ax2.set_ylabel("Gain (dB)")
+    ax3.set_ylabel("Phase (rad)")
+    ax3.set_xlabel(freq_axis_label)
+
+    for idx_band in range(num_bands):
+        color = colors[idx_band] if colors is not None else None
+        label = "band index %d" % (idx_band)
+        ax1.plot(
+            w_bank[idx_band], abs_h_bank[idx_band], color=color, label=label
+        )
+        ax2.plot(
+            w_bank[idx_band], abs_db_h_bank[idx_band], color=color, label=label
+        )
+        ax3.plot(
+            w_bank[idx_band], angle_h_bank[idx_band], color=color, label=label
+        )
+
+    ax1.legend(loc='best')
+    ax2.legend(loc='best')
+    ax3.legend(loc='best')
+
+    ax1.grid(True)
+    ax2.grid(True)
+    ax3.grid(True)
+
+    ax1.set_xscale(xscale)
+    ax1.set_xlim([xlim_lo, xlim_hi])
+    ax1.set_ylim([0.0, 1.0])
+    ax2.set_ylim([-5.0, 0.0])
+    ax3.set_ylim([-np.pi, +np.pi])
+
+    fig.tight_layout()
+    plt.show()
+
+    return
+
+
+def filter_multi_bands(sos_bank: np.ndarray, x: np.ndarray) -> np.ndarray:
+
+    num_old_chnls, num_samples = x.shape
+    num_bands = len(sos_bank)
+
+    x_bp = np.zeros((num_old_chnls, num_bands, num_samples), dtype=np.float32)
+    for idx_band in range(num_bands):
+        x_bp[:, idx_band] = ssg.sosfilt(sos_bank[idx_band], x, axis=-1)
+
+    return x_bp
+
+
+def full_rectify(x: np.ndarray) -> np.ndarray:
+    return np.abs(x)
+
+
+def filter_multi_bands_multi_channel(
+    x: np.ndarray,
+    f_hz: float,
+    bands_hz_list: list[float],
+    order: int,
+    bandplot: bool = False,
+    bandplot_norm_freq: bool = False,
+    bandplot_num_freqs: int | None = None,
+    bandplot_xscale: str = 'log',
+    bandplot_colors: list[str | tuple[float, float, float]] | None = None,
+) -> np.ndarray:
+
+    sos_bank, z_bank, p_bank, k_bank = create_butterworth_bank(
+        f_hz=f_hz, bands_hz_list=bands_hz_list, order=order,
+    )
+    if bandplot:
+        visualize_bandfilter_bank(
+            z_bank,
+            p_bank,
+            k_bank,
+            norm_freq=bandplot_norm_freq,
+            fs_hz=f_hz,
+            num_freqs=bandplot_num_freqs,
+            xscale=bandplot_xscale,
+            colors=bandplot_colors,
+        )
+
+    x = filter_multi_bands(sos_bank=sos_bank, x=x)
+    x = full_rectify(x)
+
+    return x
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ event_based_semg_regression/cochlear/lif.py
@@ -0,0 +1,203 @@
+from __future__ import annotations
+
+import numpy as np
+import brian2 as b2
+
+from semg_spike_regression.dataset import ninaprodb8 as db8
+from semg_spike_regression.cochlear import bands as bds
+
+NUM_NEURONS = db8.NUM_CHANNELS_X * bds.NUM_BANDS
+
+GAIN_DATA2XDRIVE = 3.0e5
+X_INIT = 0.0
+X_RESET = 0.5      # Kubanek: 0.5 (i.e., -65.0mV in his units)
+TAU_S = 0.010      # Kubanek: 10ms
+TREFR_S = 0.002    # Kubanek:  2ms; Elisa: do not go above 2ms.
+
+
+def data2xdrive(data: np.ndarray, gain_data2xdrive: float = 1.0):
+    x_drive = data * gain_data2xdrive
+    return x_drive
+
+
+def lif_presynaptic(
+    x_drive: np.ndarray,
+    fs_hz: float,
+    dt_sim_s: float,
+    monitors_dt_s: float,
+    x_init: float = X_INIT,
+    x_reset: float = X_RESET,
+    tau_s: float = TAU_S,
+    trefr_s: float = TREFR_S,
+    report: str | None = None,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+
+    b2.start_scope()
+
+    # convert all time and frequency arguments to Brian2 physical quantities
+    fs = fs_hz * b2.hertz
+    dt_sim = dt_sim_s * b2.second
+    monitors_dt = monitors_dt_s * b2.second
+    tau = tau_s * b2.second
+    trefr = trefr_s * b2.second
+
+    del fs_hz, dt_sim_s, monitors_dt_s, tau_s, trefr_s
+
+    num_samples = x_drive.shape[2]
+    time_total = num_samples / fs
+    dt_sample = 1.0 / fs
+
+    x_drive = x_drive.reshape((NUM_NEURONS, num_samples))
+    x_drive = b2.TimedArray(values=x_drive.T, dt=dt_sample)  # transposed!
+
+    eqn = \
+        '''
+        dx/dt = (-x + x_drive(t, i)) / tau : 1 (unless refractory)
+        '''
+
+    neurongroup = b2.NeuronGroup(
+        N=NUM_NEURONS,
+        model=eqn,
+        method='exact',
+        threshold='x > 1',
+        reset='x = x_reset',
+        refractory=trefr,
+        dtype=np.float32,
+        dt=dt_sim,
+    )
+
+    neurongroup.x = x_init
+
+    statemonitor = b2.StateMonitor(
+        source=neurongroup,
+        variables='x',
+        record=True,  # "True" monitors all indices
+        dt=monitors_dt,
+    )
+
+    # spike monitor
+    spikemonitor = b2.SpikeMonitor(
+        source=neurongroup,
+        variables=None,  # none in addition to timestamp and spiker's index
+        record=True,  # "True" monitors all indices
+    )
+
+    # (the net is created just for the scheduling summary)
+    network = b2.Network(neurongroup, statemonitor, spikemonitor)
+    scheduling_summary = network.scheduling_summary()
+    del network
+    print(f"\n\n{scheduling_summary}\n\n")
+
+    report_period = 5.0 * b2.second
+    b2.run(duration=time_total, report=report, report_period=report_period)
+
+    x = statemonitor.x
+    spike_times_s = np.float32(spikemonitor.t / b2.second)
+    spike_neuron_ids = np.uint16(spikemonitor.i)
+    # check whether sorted, and sort if needed
+    # (I do not know if they are created sorted by design from the run; it
+    # looks so, but I enforce it to be sure.)
+    nonproper_isis_s = np.diff(spike_times_s)
+    sorted = np.all(nonproper_isis_s >= 0.0)
+    if not sorted:
+        sort_idxs = np.argsort(spike_times_s)
+        pike_times_s = spike_times_s[sort_idxs]
+        spike_neuron_ids = spike_neuron_ids[sort_idxs]
+
+    return x, spike_times_s, spike_neuron_ids
+
+
+def lif_postsynaptic(
+    inspike_times_s: np.ndarray,
+    inspike_neuron_ids: np.ndarray,
+    time_total_s: float,
+    dt_sim_s: float,
+    monitor_dt_s: float,
+    tau_s: float,
+    report: str | None = None,
+) -> np.ndarray:
+
+    # sanity check: same number of spikes and spikers
+    num_spikes = len(inspike_times_s)
+    assert len(inspike_neuron_ids) == num_spikes, \
+        "Spikers' indices must be as many as the spikes' timestamps!"
+
+    # check whether sorted, and sort if needed
+    nonproper_isis_s = np.diff(inspike_times_s)
+    sorted = np.all(nonproper_isis_s >= 0.0)
+    if not sorted:
+        sort_idxs = np.argsort(inspike_times_s)
+        inspike_times_s = inspike_times_s[sort_idxs]
+        inspike_neuron_ids = inspike_neuron_ids[sort_idxs]
+
+    b2.start_scope()
+
+    # convert all time arguments to Brian2 physical quantities
+    inspike_times = inspike_times_s * b2.second
+    time_total = time_total_s * b2.second
+    dt_sim = dt_sim_s * b2.second
+    monitor_dt = monitor_dt_s * b2.second
+    tau = tau_s * b2.second
+    del inspike_times_s, time_total_s, dt_sim_s, monitor_dt_s, tau_s
+
+    spikegengroup = b2.SpikeGeneratorGroup(
+        N=NUM_NEURONS,
+        indices=inspike_neuron_ids,
+        times=inspike_times,
+        dt=dt_sim,
+        when='thresholds',
+        sorted=True,  # because they have just been sorted prior to this
+    )
+
+    eqn = \
+        '''
+        dx/dt = - x / tau : 1
+        '''
+
+    neurongroup = b2.NeuronGroup(
+        N=NUM_NEURONS,
+        model=eqn,
+        method='exact',
+        dtype=np.float32,
+        dt=dt_sim,
+    )
+
+    synapses = b2.Synapses(
+        source=spikegengroup,
+        target=neurongroup,
+        on_pre='x += 1.0',
+        dtype=np.float32,
+        dt=dt_sim,
+    )
+    synapses.connect(j='i')
+
+    neurongroup.x = 0.0
+
+    statemonitor = b2.StateMonitor(
+        source=neurongroup,
+        variables='x',
+        record=True,  # "True" monitors all indices
+        dt=monitor_dt,
+        when='end',
+    )
+
+    # (the net is created just for the scheduling summary)
+    network = b2.Network(neurongroup, synapses, statemonitor)
+    scheduling_summary = network.scheduling_summary()
+    del network
+    print(f"\n\n{scheduling_summary}\n\n")
+
+    report_period = 5.0 * b2.second
+    b2.run(duration=time_total, report=report, report_period=report_period)
+
+    x = statemonitor.x[:, 1:]  # discard the first because it is time 0.0
+
+    return x
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
